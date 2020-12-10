05deba75519a92ce46c7ccede29c9114bf522de0
hyerim-mmc <rim7951@hanyang.ac.kr>
2020-12-10 19:17:25 +0900

    final

--- test.py
+++ test.py
@@ -1,76 +1,68 @@
-import sys
 import gym
-import pylab
-import random
 import numpy as np
-from collections import deque
-import tensorflow as tf
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.initializers import RandomUniform
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-# 상태가 입력, 큐함수가 출력인 인공신경망 생성
-class DQN(tf.keras.Model):
-    def __init__(self, action_size):
-        super(DQN, self).__init__()
-        self.fc1 = Dense(24, activation='relu')
-        self.fc2 = Dense(24, activation='relu')
-        self.fc_out = Dense(action_size,
-                            kernel_initializer=RandomUniform(-1e-3, 1e-3))
+class test_Qnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        h = 256
+        self.fc1 = nn.Linear(8, h)
+        self.fc2 = nn.Linear(h, h)
+        self.fc3 = nn.Linear(h, 2)
 
-    def call(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
-        q = self.fc_out(x)
-        return q
+    # you have to change options according to train activation function (it's a hassle)
+    def forward(self, x):
+        # # relu
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
+        # x = self.fc3(x)
+
+        # sigmoid
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        x = self.fc3(x)
+
+        # # softmax
+        # x = F.softmax(self.fc1(x), dim=0)
+        # x = F.softmax(self.fc2(x), dim=0)
+        # x = self.fc3(x)
+
+        return x
 
 
-# 카트폴 예제에서의 DQN 에이전트
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        # 상태와 행동의 크기 정의
-        self.state_size = state_size
-        self.action_size = action_size
+def test():
+    env = gym.make('CartPole-v0')
+    render = True
 
-        # 모델과 타깃 모델 생성
-        self.model = DQN(action_size)
-        self.model.load_weights("./save_model/trained/model")
+    net = test_Qnet()
+    net.load_state_dict(torch.load('./result/model/model_CartPole-v0_double_sigmoid_256_20201208-18_36_24.pth'))
+    net.eval()
 
-    # 입실론 탐욕 정책으로 행동 선택
-    def get_action(self, state):
-        q_value = self.model(state)
-        return np.argmax(q_value[0])
+    s_cur = env.reset()
+    s_prev = s_cur
+    score = 0
+
+    while True:
+        if render:
+            env.render()
+        x = torch.from_numpy(np.concatenate((s_cur, s_cur - s_prev))).float()
+        q = net(x.view(1, -1)).squeeze()
+        qmax, a = torch.max(q, 0)
+        a = a.item()
+        s_prev = s_cur
+        s_cur, r, done, _ = env.step(a)
+        score += r
+
+        if done:
+            break
+
+    env.close()
+    print("Test score: {}".format(score))
 
 
 if __name__ == "__main__":
-    # CartPole-v1 환경, 최대 타임스텝 수가 500
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-
-    # DQN 에이전트 생성
-    agent = DQNAgent(state_size, action_size)
-
-    num_episode = 10
-    for e in range(num_episode):
-        done = False
-        score = 0
-        # env 초기화
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
-
-        while not done:
-            env.render()
-
-            # 현재 상태로 행동을 선택
-            action = agent.get_action(state)
-            # 선택한 행동으로 환경에서 한 타임스텝 진행
-            next_state, reward, done, info = env.step(action)
-            next_state = np.reshape(next_state, [1, state_size])
-
-            score += reward
-            state = next_state
-
-            if done:
-                # 에피소드마다 학습 결과 출력
-                print("episode: {:3d} | score: {:.3f} ".format(e, score))
+    test()
---
+++ train.py
@@ -0,0 +1,237 @@
+# >> tensorboard --logdir=./tensorboard
+import json
+import gym
+import datetime
+import collections
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Json_Parser:
+    def __init__(self, file_name):
+        with open(file_name) as json_file:
+            self.json_data = json.load(json_file)
+
+    def load_parser(self):
+        return self.json_data
+
+
+class Qnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.parser = Json_Parser("config.json")
+        h = self.parser.load_parser()['agent']['hidden_unit']
+        self.fc1 = nn.Linear(8, h)
+        self.fc2 = nn.Linear(h, h)
+        self.fc3 = nn.Linear(h, 2)
+
+    def forward(self, x):
+        acti = self.parser.load_parser()['agent']['activation']
+        if acti == "relu":
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+        elif acti == "sigmoid":
+            x = torch.sigmoid(self.fc1(x))
+            x = torch.sigmoid(self.fc2(x))
+            x = self.fc3(x)
+        elif acti == "softmax":
+            x = F.softmax(self.fc1(x), dim=0)
+            x = F.softmax(self.fc2(x), dim=0)
+            x = self.fc3(x)
+        return x
+
+
+class ReplayMemory:
+    def __init__(self, memory_size, keys):
+        self.memory = {}
+        for key in keys:
+            self.memory[key] = collections.deque(maxlen=memory_size)
+        self.memory_size = memory_size
+
+    def save(self, observations):
+        for i, key in enumerate(self.memory.keys()):
+            self.memory[key].append(observations[i])
+
+    def __len__(self):
+        return len(self.memory['x'])
+
+    def sample(self, idx):
+        sub_memory = {}
+        for key in self.memory.keys():
+            sub_memory[key] = [self.memory[key][i] for i in idx]
+
+        ss, actions, rs, ss_next, dones = sub_memory.values()
+        ss = torch.stack(ss)
+        ss_next = torch.stack(ss_next)
+        rs = np.array(rs)
+        rs = torch.from_numpy(rs).float()
+
+        return (ss, actions, rs, ss_next, dones)
+
+
+class DQNAgent:
+    def __init__(self):
+        super().__init__()
+        
+        date_time = datetime.datetime.now().strftime("%Y%m%d-%H_%M_%S")
+        self.parser = Json_Parser("config.json")
+        self.parm = self.parser.load_parser()
+        self.method = self.parm['method']
+        self.max_step = self.parm['max_step']
+        self.discount_factor = self.parm['agent']['discount_factor']
+        self.lr = self.parm['optimizer']['learning_rate']
+        self.eps = self.parm['optimizer']['eps']
+        self.eps_max = self.eps
+        self.eps_min = self.parm['optimizer']['eps_min']
+        self.eps_mid = self.parm['optimizer']['eps_mid']
+        self.eps_anneal = self.parm['optimizer']['eps_anneal']
+        self.episode_size = self.parm['episode_size']
+        self.minibatch_size = self.parm['minibatch_size']
+        self.net_update_period = self.parm['net_update_period']
+
+        self.env = gym.make('{}'.format(self.parm['env_name']))
+        self.net = Qnet()
+        self.target_net = Qnet()
+        self.target_net.load_state_dict(self.net.state_dict())
+        self.target_net.eval()
+
+        self.replay_memory = ReplayMemory(self.parm['memory_size'], keys=[
+                                          'x', 'a', 'r', 'x_next', 'done'])
+        self.optimizer = optim.Adam(
+            self.net.parameters(), lr=self.lr, weight_decay=0)
+        self.loss = nn.SmoothL1Loss()
+
+        save_name = self.parm['env_name'] + '_' + self.method + '_' + self.parm['agent']['activation'] + '_' + \
+            str(self.parm['agent']['hidden_unit']) + '_' + date_time
+        self.writer = SummaryWriter('./result/tensorboard/' + save_name)
+        self.net_save_path = './result/model/model_{}.pth'.format(save_name)
+        self.writer.add_text('config', json.dumps(self.parm))
+
+    def get_action(self, x):
+        if np.random.rand() < self.eps:
+            action = np.random.randint(2)
+        else:
+            self.net.eval()
+            q = self.net(x.view(1, -1))
+            action = np.argmax(q.detach().numpy())
+        return action
+
+    def epsilon_decaying(self):
+        if self.eps > self.eps_mid:
+            self.eps -= (self.eps_max-self.eps_mid)/self.eps_anneal
+        if self.eps < self.eps_mid and self.eps > self.eps_min:
+            self.eps -= (self.eps_mid-self.eps_min)/self.eps_anneal
+
+    def train(self, running_loss):
+        self.epsilon_decaying()
+
+        self.net.train()
+        minibatch_idx = np.random.choice(self.replay_memory.__len__(), self.minibatch_size)
+        ss, actions, rs, ss_next, dones = self.replay_memory.sample(minibatch_idx)
+        final_state_idx = np.nonzero(dones)
+
+        if self.method == "double":
+            with torch.no_grad():
+                self.net.eval()
+                q_next = self.net(ss_next)
+                q_next_ = self.target_net(ss_next)
+
+            self.net.train()
+            self.optimizer.zero_grad()
+            q = self.net(ss)
+            q_next_max, q_next_argmax = torch.max(q_next, 1)
+            v_next = torch.gather(q_next_, 1, q_next_argmax.view(-1, 1)).squeeze()
+
+        if self.method == "vanilla":
+            with torch.no_grad():
+                q_next = self.target_net(ss_next)
+
+            self.optimizer.zero_grad()
+            q = self.net(ss)
+            q_next_max, q_next_argmax = torch.max(q_next, 1)
+            v_next = q_next_max
+
+        v_next[final_state_idx] = 0
+        q_target = rs + self.discount_factor*v_next
+        actions = torch.tensor(actions).view(-1, 1)
+        q_relevant = torch.gather(q, 1, actions).squeeze()
+
+        loss = self.loss(q_relevant, q_target)
+        loss.backward()
+        self.optimizer.step()
+
+        running_loss = loss.item() if running_loss == 0 else 0.99 * \
+            running_loss + 0.01*loss.item()
+
+        return running_loss
+
+    def run(self):
+        backprops_total = 0
+        running_loss = 0
+        latest_scores = collections.deque(maxlen=100)
+        pass_score = self.max_step - 4
+
+        s_now = self.env.reset()
+        s_prev = s_now
+        score = 0
+
+        for episode in range(self.episode_size):
+            episode += 1
+            for step in range(self.max_step):
+                x = torch.from_numpy(np.concatenate(
+                    (s_now, s_now-s_prev))).float()
+                a = self.get_action(x)
+
+                s_next, r, done, _ = self.env.step(a)
+                score += 1
+
+                x_next = torch.from_numpy(
+                    np.concatenate((s_next, s_next-s_now))).float()
+                self.replay_memory.save((x, a, r, x_next, done))
+
+                if done:
+                    latest_scores.append(score)
+                    score = 0
+                    s_now = self.env.reset()
+                    s_prev = s_now
+                else:
+                    s_prev = s_now
+                    s_now = s_next
+
+                if self.replay_memory.__len__() > self.minibatch_size:
+                    running_loss = self.train(running_loss)
+                    backprops_total += 1
+
+                self.writer.add_scalar('memory_size', self.replay_memory.__len__(), episode)
+                self.writer.add_scalar('epsilon', self.eps, episode)
+                self.writer.add_scalar('running_loss', running_loss, episode)
+                self.writer.add_scalar('avg_score', np.mean(latest_scores), episode)
+
+                if backprops_total % self.net_update_period == 0:
+                    self.target_net.load_state_dict(self.net.state_dict())
+
+                if done and episode % 100 == 0:
+                    print("episode: {} | memory_size: {:5d} | eps: {:.3f} | running_loss: {:.3f} | last 100 avg score: {:3.1f}".
+                          format(episode, self.replay_memory.__len__(), self.eps, running_loss, np.mean(latest_scores)))
+                    torch.save(self.net.state_dict(), self.net_save_path)
+
+                    if np.mean(latest_scores) > pass_score:
+                        print('latest 100 average score: {}, pass score: {}, test is passed'.format(
+                            np.mean(latest_scores), pass_score))
+                        exit(0)
+
+                if done:
+                    break
+
+        self.env.close()
+
+
+if __name__ == "__main__":
+    agent = DQNAgent()
+    agent.run()
